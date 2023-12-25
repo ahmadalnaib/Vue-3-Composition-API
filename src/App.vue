5a322381eed7f887cab8ecaae584548d3dd35508
ahmedhabeeb73 <alnaib888@gmail.com>
2023-12-25 11:36:44 +0100

    add tran

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@
   <Balance />
   <IncomeExpense />
   <TransactionList />
+  <AddTransaction />
 
  </div>
 </template>
@@ -15,6 +16,7 @@ import Header from './components/Header.vue'
 import Balance from './components/Balance.vue'
 import IncomeExpense from './components/IncomeExpense.vue'
 import TransactionList from './components/TransactionList.vue';
+import AddTransaction from './components/AddTransaction.vue'
 
 
 </script>
