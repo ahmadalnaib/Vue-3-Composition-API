5b9eebedc9485456b53a80bae77ca17a2266404c
ahmedhabeeb73 <alnaib888@gmail.com>
2023-12-25 11:29:30 +0100

    add the layout

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,20 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+ <Header />
+ <div class="container">
+  <Balance />
+  <IncomeExpense />
+  <TransactionList />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+ </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
+<script setup>
+import Header from './components/Header.vue'
+import Balance from './components/Balance.vue'
+import IncomeExpense from './components/IncomeExpense.vue'
+import TransactionList from './components/TransactionList.vue';
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+</script>
